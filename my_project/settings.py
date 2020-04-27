2ff27a2bd3b7dd0f374ffc5f5c408f1e292d8a60
140ch204 <140ch204_github>
2020-04-27 14:03:19 +0200

    init

--- my_project/settings.py
+++ my_project/settings.py
@@ -17,12 +17,16 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wv)7$!z@=h^#ryf=wq5s=30sv8f174*g+=38o7q3j3w21mx$ns'
+SECRET_KEY = os.getenv("ENV_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
